d00f0b8c576e585d9f61e7f4d0f1c7124028bc4d
uni <uni@unikimPC>
2023-02-18 13:18:21 +0900

    테스트 커밋_230218

---
+++ GoguMarket/src/main/java/com/goguma/prj/GoguMarketApplication.java
@@ -0,0 +1,13 @@
+package com.goguma.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoguMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoguMarketApplication.class, args);
+	}
+
+}
