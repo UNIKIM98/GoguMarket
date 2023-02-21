e867fe78fef48e871148be33c96a5d39037abfa6
kwonhyogeun <khgkhgk@naver.com>
2023-02-21 17:35:27 +0900

    0221_hyogeun

--- GoguMarket/src/main/java/com/goguma/auct/controller/AuctController.java
+++ GoguMarket/src/main/java/com/goguma/auct/controller/AuctController.java
@@ -1,5 +1,20 @@
 package com.goguma.auct.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuctController {
 
+	@GetMapping("/auctSelectList")
+	public String auctSelectList() {
+		//효근
+		return "auction/auctSelectList";
+	}
+	
+	@GetMapping("/auctSelect")
+	public String auctSelect() {
+		//효근
+		return "auction/auctSelect";
+	}
 }
