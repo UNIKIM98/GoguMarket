c5eb71717b4995a20a79941d75b0b57051a1a616
kwonhyogeun <khgkhgk@naver.com>
2023-02-22 17:11:44 +0900

    0222_hyogeun

--- GoguMarket/src/main/java/com/goguma/auct/service/AuctService.java
+++ GoguMarket/src/main/java/com/goguma/auct/service/AuctService.java
@@ -1,5 +1,14 @@
 package com.goguma.auct.service;
 
-public interface AuctService {
+import java.util.List;
 
+import com.goguma.auct.vo.AuctVO;
+
+public interface AuctService {
+	List<AuctVO> getAuctList(AuctVO vo);
+
+	AuctVO getAuct(String id); // 사원검색
+	int insertAuct(AuctVO auct); // 등록
+	int updateAuct(AuctVO auct); // 수정
+	int deleteAuct(String id); // 삭제
 }
--- GoguMarket/src/main/java/com/goguma/auct/serviceImpl/AuctServiceImpl.java
+++ GoguMarket/src/main/java/com/goguma/auct/serviceImpl/AuctServiceImpl.java
@@ -1,7 +1,46 @@
 package com.goguma.auct.serviceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.goguma.auct.mapper.AuctMapper;
 import com.goguma.auct.service.AuctService;
+import com.goguma.auct.vo.AuctVO;
 
 public class AuctServiceImpl implements AuctService{
+	@Autowired
+	AuctMapper auctMapper;
 
+	@Override
+	public List<AuctVO> getAuctList(AuctVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AuctVO getAuct(String id) {
+		// TODO Auto-generated method stub
+		return auctMapper.getAuct(id);
+	}
+
+	@Override
+	public int insertAuct(AuctVO auct) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateAuct(AuctVO auct) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteAuct(String id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 }
