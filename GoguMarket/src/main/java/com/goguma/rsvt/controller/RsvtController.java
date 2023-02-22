99b6a33f70523e2534605c83f8ec8f5432d339b6
Lee jiyeong <wldud920313@naver.com>
2023-02-22 17:28:55 +0900

    230222_1728 ~book06-4, mybook04 화면구현

--- GoguMarket/src/main/java/com/goguma/rsvt/controller/RsvtController.java
+++ GoguMarket/src/main/java/com/goguma/rsvt/controller/RsvtController.java
@@ -1,5 +1,51 @@
 package com.goguma.rsvt.controller;
 
-public class RsvtController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.goguma.rsvt.service.RsvtService;
+
+@Controller
+public class RsvtController {
+	//@Autowired
+	//private RsvtService rsvtService;
+	
+	@GetMapping("/book01")	//동네가게 예약 메인
+	public String book01() {
+		return "rsvt/book01";
+	}
+	
+	@GetMapping("/book0205")
+	public String book0205() {
+		return "rsvt/book0205";
+	}
+	
+	@GetMapping("/book0601")
+	public String book0601() {
+		return "rsvt/book0601";
+	}
+	
+	@GetMapping("/book0602")
+	public String book0602() {
+		return "rsvt/book0602";
+	}
+	
+	@GetMapping("/book0603")
+	public String book0603() {
+		return "rsvt/book0603";
+	}
+	
+	@GetMapping("/book0604")
+	public String book0604() {
+		return "rsvt/book0604";
+	}
+	
+	@GetMapping("/mybook04")
+	public String mybook04() {
+		return "rsvt/mybook04";
+	}
+	
 }
+
