fb79a199ba739ea7bc613ab4616c0cc0f89a1cf1
UNIKIM <uni_ary@naver.com>
2023-02-22 16:41:05 +0900

    230222 16:40 chaeun

--- GoguMarket/src/main/java/com/goguma/mem/controller/MemController.java
+++ GoguMarket/src/main/java/com/goguma/mem/controller/MemController.java
@@ -1,5 +1,13 @@
 package com.goguma.mem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemController {
 
+	@GetMapping("/myPageTest")
+	public String myPageTest() {
+		return "myPages/test";
+	}
 }
