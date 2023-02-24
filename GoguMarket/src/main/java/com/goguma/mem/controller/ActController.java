a33b06347a7a08139b8bcf7972daa9fe828a1cc1
uni <uni@unikimPC>
2023-02-25 01:47:06 +0900

    0224 chaeun

--- GoguMarket/src/main/java/com/goguma/mem/controller/ActController.java
+++ GoguMarket/src/main/java/com/goguma/mem/controller/ActController.java
@@ -1,8 +1,6 @@
 package com.goguma.mem.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
