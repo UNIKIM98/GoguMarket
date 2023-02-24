a405c64c5ac494420f6d80e5263d8d75bffade0e
Lee jiyeong <wldud920313@naver.com>
2023-02-24 17:27:58 +0900

    230224_1727 화면구현완료, 예약메인 mapper, service 등등

--- GoguMarket/src/main/java/com/goguma/rsvt/service/RsvtService.java
+++ GoguMarket/src/main/java/com/goguma/rsvt/service/RsvtService.java
@@ -1,5 +1,10 @@
 package com.goguma.rsvt.service;
 
-public interface RsvtService {
+import java.util.List;
 
+import com.goguma.biz.vo.BizMemVO;
+
+public interface RsvtService {
+	//전체 가게 조회
+		List<BizMemVO> getBizList();
 }
