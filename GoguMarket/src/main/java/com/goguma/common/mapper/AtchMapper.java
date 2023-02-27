a5f446cf87f5df2df84c948e6b8f45efaf2e9a2c
yeaseul <yeaseul1217@gmail.com>
2023-02-27 15:56:00 +0900

    co

--- GoguMarket/src/main/java/com/goguma/common/mapper/AtchMapper.java
+++ GoguMarket/src/main/java/com/goguma/common/mapper/AtchMapper.java
@@ -2,12 +2,14 @@ package com.goguma.common.mapper;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.goguma.common.vo.AtchVO;
+
 public interface AtchMapper {
 	// ▶ atchId 구하는 쿼리문
 	int selectAtchId();
 
 	// ▶ 파일 다중 업로드 > atchId 리턴
-	int fileUpload(MultipartFile file);
+	int fileUpload(AtchVO file);
 
 	// ▶ 파일 삭제
 
