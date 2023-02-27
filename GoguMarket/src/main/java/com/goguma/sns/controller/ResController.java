5e18cb3a7ec9255e02fdc190c965052e2bdbb74c
jaeyeop <jy4286@hanmail.net>
2023-02-27 17:37:10 +0900

    ><

--- GoguMarket/src/main/java/com/goguma/sns/controller/ResController.java
+++ GoguMarket/src/main/java/com/goguma/sns/controller/ResController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,11 +35,10 @@ public class ResController {
 	}
 
 	@GetMapping("/selectSns")
-	@ResponseBody
-	public Map<String, Object> getSns(int id) {
-		System.out.println(id);
+	public Map<String, Object> getSns(@PathVariable int snsId) {
+		System.out.println(snsId);
 	
-		SnsVO vo = service.selectSns(id);
+		SnsVO vo = service.selectSns(snsId);
 		 
 		Map<String, Object> map = new HashMap<String, Object>();
 
