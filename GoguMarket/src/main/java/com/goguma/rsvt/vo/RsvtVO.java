2344236a1d32e9c9469272f05e51ea9faa284dad
Lee jiyeong <wldud920313@naver.com>
2023-03-06 19:37:57 +0900

    230306_예약페이지

--- GoguMarket/src/main/java/com/goguma/rsvt/vo/RsvtVO.java
+++ GoguMarket/src/main/java/com/goguma/rsvt/vo/RsvtVO.java
@@ -1,6 +1,8 @@
 package com.goguma.rsvt.vo;
 
 import java.sql.Date;
+import java.util.List;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
@@ -11,9 +13,9 @@ public class RsvtVO extends RsvtRvVO{	//예약내역 테이블
 	String bizNo;			//가게번호
 	String userId;			//예약자id(사용자id)
 	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	//@DateTimeFormat(pattern = "yyyy-MM-dd") -> sql.Date는 이거 사용x
 	Date rsvtYmd;			//예약날짜
-	Date rsvtTm;			//예약시간
+	String rsvtTm;			//예약시간
 	int rsvtNope;			//인원
 	String rsvtWant;		//요청사항
 	int rsvtPay;			//결제금액
@@ -22,4 +24,6 @@ public class RsvtVO extends RsvtRvVO{	//예약내역 테이블
 	String comOrderYn;		//함께 주문 여부
 	String payMthhd;		//결제수단
 	String alarmYn;			//알림여부
+	
+	
 }
