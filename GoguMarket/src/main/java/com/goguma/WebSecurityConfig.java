6461156686c5a8df7c67a9d212c8dae21f1d012c
UNIKIM <uni_ary@naver.com>
2023-03-06 19:37:58 +0900

    CHAEUN

---
+++ GoguMarket/src/main/java/com/goguma/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.goguma;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	@Bean
+	private BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+}
+	
+	@Bean 
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(
+				(requests) -> requests.antMatchers("/", "/home").permitAll().anyRequest().authenticated())
+				.formLogin((form) -> form.successHandler(new LoginSuccessHandler())
+						).logout((logout) -> logout.permitAll())
+				.csrf().disable();
+
+		return http.build();
+	}
+
+}
