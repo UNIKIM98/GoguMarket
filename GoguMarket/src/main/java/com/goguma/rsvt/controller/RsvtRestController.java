d97edd682939e31721fb08050a64f218667a4729
leeejiyeong <wldud920313@naver.com>
2023-03-08 02:37:32 +0900

    230308_0237 예약페이지 메뉴 삭제, 결제api

--- GoguMarket/src/main/java/com/goguma/rsvt/controller/RsvtRestController.java
+++ GoguMarket/src/main/java/com/goguma/rsvt/controller/RsvtRestController.java
@@ -1,14 +1,26 @@
 package com.goguma.rsvt.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.goguma.biz.service.BizMemService;
+import com.goguma.rsvt.service.RsvtMenuService;
+import com.goguma.rsvt.service.RsvtPaymentService;
+import com.goguma.rsvt.service.RsvtService;
+import com.goguma.rsvt.vo.RsvtPaymentVO;
 
 @RestController
 public class RsvtRestController {
 	
-	@Autowired BizMemService memService; 	// 가게정보
+	@Autowired RsvtService rsvtService;
+	
+	@GetMapping("/insertRsvtPayment")
+	public List<RsvtPaymentVO> insertRsvtPayment(){
+//		List<RsvtPaymentVO> result 
+		return null;
+		
+	}
 	
 }
