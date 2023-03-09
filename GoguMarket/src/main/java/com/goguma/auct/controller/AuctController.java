5e7eae41d6960ad5b6cd27eb747fac01752e4740
kwonhyogeun <khgkhgk@naver.com>
2023-03-09 19:24:43 +0900

    0309_효근 학원

--- GoguMarket/src/main/java/com/goguma/auct/controller/AuctController.java
+++ GoguMarket/src/main/java/com/goguma/auct/controller/AuctController.java
@@ -9,13 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.event.PublicInvocationEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -180,10 +178,5 @@ public class AuctController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	@GetMapping("/my/auctExpired")
-		public String auctExpired() {
-		return "/my/auctExpired";
 	}
 }
