8e240edf8d595ab88e6b40118f63a81d205b1baa
Lee jiyeong <wldud920313@naver.com>
2023-03-13 18:32:41 +0900

    230313_1832 비즈페이지ing

--- GoguMarket/src/main/java/com/goguma/rsvt/serviceImpl/RsvtRvServiceImpl.java
+++ GoguMarket/src/main/java/com/goguma/rsvt/serviceImpl/RsvtRvServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.goguma.biz.vo.BizSearchVO;
 import com.goguma.rsvt.mapper.RsvtRvMapper;
 import com.goguma.rsvt.service.RsvtRvService;
 import com.goguma.rsvt.vo.RsvtRvVO;
@@ -19,6 +20,18 @@ public class RsvtRvServiceImpl implements RsvtRvService{
 	@Override
 	public List<RsvtRvVO> rsvtReview(String bizNo) {
 		return map.rsvtReview(bizNo);
+	}
+
+	//비즈페이지 후기리스트
+	@Override
+	public List<RsvtRvVO> selectReviewList(String bizNo) {
+		return map.selectReviewList(bizNo);
+	}
+	
+	//비즈페이지 후기리스트 카운팅
+	@Override
+	public int selectReviewCnt(String bizNo) {
+		return map.selectReviewCnt(bizNo);
 	}
 	
 }
