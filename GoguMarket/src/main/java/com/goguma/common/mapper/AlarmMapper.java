69f38f82fa7c7c9c57abb809dd8be72bd9853c89
User <User@DESKTOP-RD402RC>
2023-03-14 11:31:35 +0900

    2023-03-14 wqwddasdas

--- GoguMarket/src/main/java/com/goguma/common/mapper/AlarmMapper.java
+++ GoguMarket/src/main/java/com/goguma/common/mapper/AlarmMapper.java
@@ -1,9 +1,18 @@
 package com.goguma.common.mapper;
 
+import java.util.List;
+
 import com.goguma.common.vo.AlarmVO;
 
 public interface AlarmMapper {
 
 	int insertAlarm(AlarmVO vo);
+	
+	int checkNotifyCount(AlarmVO vo);
+	
+	List<AlarmVO> selectNotify(AlarmVO vo);
+	
+	boolean updateNotify(AlarmVO vo);
+	
 	
 }
